d1e9921db7f29d717b51bb17fee1790b5cf5e35a
8ball030 <8baller@station.codes>
2026-07-30 20:37:57 +0100

    feat:initial-scaffold

--- src/namespaces/fund_movements.rs
+++ src/namespaces/fund_movements.rs
@@ -119,4 +119,35 @@ impl<'a> FundMovementsNamespace<'a> {
             .transfer_spot(params)
             .await
     }
+
+    pub async fn transfer_position(&self, args: PositionTransferArgs) -> Result<PrivatePositionTransferResponse, ClientError> {
+        let signer = self.ws_client.wallet.clone().unwrap();
+
+        let wallet = self
+            .ws_client
+            .smart_contract_wallet_address
+            .clone()
+            .unwrap();
+        let env = &self.ws_client.environment;
+
+        let data = PositionTransferData::from_args(args.clone())?;
+        let action = ActionData::new(
+            data,
+            args.subaccount_id,
+            signer.address(),
+            &wallet.parse().expect("Couldnt parse wallet address"),
+            &self.ws_client.environment,
+            ModuleType::PositionTransfer,
+        )?;
+
+        let params = action.populate_transfer_position_params(&signer, args.clone(), env)?;
+
+        println!("Transfer Position params: {:?}", params);
+
+        self.ws_client
+            .rpc()
+            .transfers_withdrawals()
+            .transfer_position(params)
+            .await
+    }
 }
